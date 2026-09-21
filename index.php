a286ccec0735f79700c70778bbc6e38bef0e9fc1
nawawi123456 <nnnawawiii@gmail.com>
2026-09-21 11:21:29 +0700

    update

--- index.php
+++ index.php
@@ -74,7 +74,8 @@ if (!file_exists($file)) {
 <script src="./libs/feather-icons/feather.min.js"></script>
 <script src="js/pages/plugins/lord-icon-2.1.0.js"></script>
 <script src="js/plugins.js"></script>
-
+<script src="./libs/flatpickr/flatpickr.min.js"></script>
+<script src="./libs/choices.js/public/assets/scripts/choices.min.js"></script>
 <!-- App js -->
 <script src="js/app.js"></script>
 
